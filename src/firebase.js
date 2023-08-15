3a7d23e48f8114812864ea466c829558b46d3124
AlexanderClanton <alexclantons@gmail.com>
2023-08-14 20:57:18 -0700

    code of vroomers. credit to my other co creators

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCFo15vpFh9wYryHDo0zRfHjiIHeU59irQ",
+  authDomain: "vroomers-351fe.firebaseapp.com",
+  databaseURL: "https://vroomers-351fe-default-rtdb.firebaseio.com",
+  projectId: "vroomers-351fe",
+  storageBucket: "vroomers-351fe.appspot.com",
+  messagingSenderId: "514797662897",
+  appId: "1:514797662897:web:74e037a6e9597d21866890",
+  measurementId: "G-19SFJ9V17N"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
+
